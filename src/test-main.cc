de1ce65550290606d5883dac05d6366659fa7756
michael <michaelgilch93@gmail.com>
2021-06-12 19:29:16 +0200

    Setup of a basic link with opencv

--- src/test-main.cc
+++ src/test-main.cc
@@ -1,8 +1,28 @@
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include <math.h>
+#include <random>
 
+#include "opencv2/core/mat.hpp"
+
+[[nodiscard]]  double  add(float a, double b)
+{
+    return std::pow(a,b);
+}
 static void BM_StringCreation(benchmark::State& state) {
+double lower_bound = 0;
+    double upper_bound = 10000;
+    std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+    std::default_random_engine re;
+    double a = unif(re);
+    double b = unif(re );
+
+cv::Mat_<double> mat(1,1);
+double d;
   for (auto _ : state)
-    std::string empty_string;
+  {
+        benchmark::DoNotOptimize(d= add(a,b) + mat.at<double>(0));
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
