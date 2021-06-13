46725c039fb0e0b5fbab817c7d64f4eda3526bee
michael <michaelgilch93@gmail.com>
2021-06-13 12:49:11 +0200

    Test of iterator performance for opencv matrix running

--- src/test-main.cc
+++ src/test-main.cc
@@ -5,34 +5,46 @@
 
 #include "opencv2/core/mat.hpp"
 
-[[nodiscard]]  double  add(float a, double b)
-{
-    return std::pow(a,b);
-}
-static void BM_StringCreation(benchmark::State& state) {
-double lower_bound = 0;
-    double upper_bound = 10000;
-    std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
-    std::default_random_engine re;
-    double a = unif(re);
-    double b = unif(re );
+template <typename T, int U>
+static void BM_AccumulateIterator(benchmark::State &state) {
+  double lower_bound = 0;
+  double upper_bound = 10000;
+  std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+  std::default_random_engine re;
 
-cv::Mat_<double> mat(1,1);
-double d;
-  for (auto _ : state)
-  {
-        benchmark::DoNotOptimize(d= add(a,b) + mat.at<double>(0));
+  cv::Mat_<T> mat(U, 1);
+  std::generate(mat.begin(), mat.end(), [&re, &unif]() { return T(unif(re)); });
+
+  T d = 0;
+  for (auto _ : state) {
+
+    d = std::accumulate(mat.begin(), mat.end(), T(0.0));
+    benchmark::DoNotOptimize(d);
+  }
+}
+
+template <typename T, int U>
+static void BM_AccumulatePointer(benchmark::State &state) {
+  double lower_bound = 0;
+  double upper_bound = 10000;
+  std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+  std::default_random_engine re;
+
+  cv::Mat_<T> mat(U, 1);
+  std::generate(mat.begin(), mat.end(), [&re, &unif]() { return T(unif(re)); });
+
+  T d = 0;
+  for (auto _ : state) {
+
+    T *beginPtr = reinterpret_cast<T *>(mat.data);
+    T *endPtr = reinterpret_cast<T *>(mat.data) + mat.cols * mat.rows;
+
+    d = std::accumulate(beginPtr, endPtr, T(0.0));
+    benchmark::DoNotOptimize(d);
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK_TEMPLATE(BM_AccumulateIterator, double, 100000);
+BENCHMARK_TEMPLATE(BM_AccumulatePointer, double, 100000);
 
 BENCHMARK_MAIN();
