50df629fd7afa47879c1626e9032cbce1e2c0be6
michael <michaelgilch93@gmail.com>
2022-01-04 21:06:26 +0100

    putting everything into an experimental namespace. Cleaning and commenting TMP functions

--- src/algorithm.hh
+++ src/algorithm.hh
@@ -7,21 +7,7 @@
 #include <tuple>
 #include <type_traits>
 #include <utility>
-
-/// Compile time loop over a tuple.Thanks to
-// https://stackoverflow.com/questions/37602057/why-isnt-a-for-loop-a-compile-time-expression
-template <std::size_t N> struct num { static const constexpr auto value = N; };
-
-template <class F, std::size_t... Is>
-void for_(F func, std::index_sequence<Is...>) {
-  using expander = int[];
-  (void)expander{0, ((void)func(num<Is>{}), 0)...};
-}
-
-template <std::size_t N, typename F> void for_(F func) {
-  for_(func, std::make_index_sequence<N>());
-}
-
+namespace experimental {
 /// This is how we loop through the tuple.
 /// We replace all instances of a cv::MatIterator  with its pointer.
 /// We recursively shave off one argument of the variadic template pack
@@ -76,33 +62,74 @@ constexpr auto make_tpl_replaced(Tpl tpl, cv::MatConstIterator_<T> arg) {
   return std::tuple_cat(tpl, tpl_ptr);
 }
 
-/// These two functions replace a c++17 if constexpr.
-/// We return true in the generic case because that's the neutral element of the
-/// && operation
-template <typename Arg> bool __isContinuous_single(Arg arg) {
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename Arg, typename... Args>
+bool __isContinuous_variadic(bool isContinuous, Arg arg, Args... args) {
   (void)arg; // unused parameter. Only used for overload resolution
-  return true;
+  return __isContinuous_variadic(isContinuous, args...);
 }
 
-/// These two functions replace a c++17 if constexpr.
-/// We return if the iterator is contiguous
-bool __isContinuous_single(cv::MatConstIterator it) {
-  return it.m->isContinuous();
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename T, typename... Args>
+std::pair<bool, bool>
+__isContinuous_variadic(std::pair<bool, bool> continuousPair,
+                        cv::MatConstIterator_<T> it, Args... args) {
+  return std::make_pair(continuousPair.first & it.m->isContinuous(), true);
+}
+
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename T, typename... Args>
+std::pair<bool, bool>
+__isContinuous_variadic(std::pair<bool, bool> continuousPair,
+                        cv::MatIterator_<T> it, Args... args) {
+  return std::make_pair(continuousPair.first & it.m->isContinuous(), true);
+}
+
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename Arg>
+std::pair<bool, bool>
+__isContinuous_variadic(std::pair<bool, bool> continuousPair, Arg arg) {
+  (void)arg; // unused parameter. Only used for overload resolution
+  return continuousPair;
+}
+
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename T>
+std::pair<bool, bool>
+__isContinuous_variadic(std::pair<bool, bool> continuousPair,
+                        cv::MatConstIterator_<T> it) {
+  return std::make_pair(continuousPair.first & it.m->isContinuous(), true);
+}
+
+/// These functions replace a c++17 if constexpr.
+/// We return if the iterator is contiguous and if we've had at least one opencv
+/// iterator
+template <typename T>
+std::pair<bool, bool>
+__isContinuous_variadic(std::pair<bool, bool> continuousPair,
+                        cv::MatIterator_<T> it) {
+  return std::make_pair(continuousPair.first & it.m->isContinuous(), true);
 }
 
 /// We loop through the touple
-template <typename Tpl> bool __isContinuous(Tpl tpl) {
-  bool isContinuous = true; // runtime
+template <typename... Args> bool __isContinuous(Args... args) {
+  bool isContinuous = true;
   bool oneElement = false;
 
-  constexpr size_t tupleSize = std::tuple_size<decltype(tpl)>::value;
+  std::pair<bool, bool> continousPair = __isContinuous_variadic(
+      std::make_pair(isContinuous, oneElement), args...);
 
-  // Here we for loop
-  for_<tupleSize>([&](auto i) {
-    isContinuous &= __isContinuous_single(std::get<i.value>(tpl));
-  });
-
-  return isContinuous && oneElement;
+  return continousPair.first && continousPair.second;
 }
 
 template <typename... Args, std::size_t... Is>
@@ -114,12 +141,17 @@ template <typename... Args> auto count_if(Args... args) {
 
   std::tuple<Args...> tpl = std::make_tuple(args...);
   constexpr size_t tupleSize = std::tuple_size<decltype(tpl)>::value;
-  bool isContinuous = __isContinuous(tpl);
+  bool isContinuous = __isContinuous(args...);
+
+  // std::cout << isContinuous << std::endl;
+
+  std::cout << isContinuous << std::endl;
 
   auto replacedTpl = make_tpl_replaced(std::tuple<>{}, args...);
-  if (!isContinuous) {
+  if (isContinuous) {
     return __count_if(replacedTpl, std::make_index_sequence<tupleSize>());
   } else {
     return std::count_if(std::forward<Args>(args)...);
   }
 }
+} // namespace experimental
