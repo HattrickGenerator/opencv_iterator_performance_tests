cb0f4a5ad6f1f82d1bf1c166b3872df4b410e9f9
michael <michaelgilch93@gmail.com>
2022-01-05 16:35:26 +0100

    Working example with 4 tests

--- src/algorithm.hh
+++ src/algorithm.hh
@@ -7,6 +7,7 @@
 #include <tuple>
 #include <type_traits>
 #include <utility>
+namespace experimental {
 
 /// Compile time loop over a tuple.Thanks to
 // https://stackoverflow.com/questions/37602057/why-isnt-a-for-loop-a-compile-time-expression
@@ -22,76 +23,63 @@ template <std::size_t N, typename F> void for_(F func) {
   for_(func, std::make_index_sequence<N>());
 }
 
+template <typename Tpl, typename Arg>
+constexpr auto make_tpl_replaced(Tpl tpl, Arg arg) {
+  if constexpr (!std::is_base_of<cv::MatConstIterator, Arg>::value) {
+    auto tpl_ptr = std::make_tuple(arg);
+    return std::tuple_cat(tpl, tpl_ptr);
+  }
+
+  else {
+    auto tpl_ptr = std::make_tuple(arg.ptr);
+    return std::tuple_cat(tpl, tpl_ptr);
+  }
+}
+
 /// This is how we loop through the tuple.
 /// We replace all instances of a cv::MatIterator  with its pointer.
 /// We recursively shave off one argument of the variadic template pack
 template <typename Tpl, typename Arg, typename... Args>
 constexpr auto make_tpl_replaced(Tpl tpl, Arg arg, Args... args) {
 
-  auto tpl_ptr = std::make_tuple(arg);
-  auto tpl_cat = std::tuple_cat(tpl, tpl_ptr);
-  return make_tpl_replaced(tpl_cat, args...); // TODO: has to be right type
-}
+  if constexpr (!std::is_base_of<cv::MatConstIterator, Arg>::value) {
+    auto tpl_ptr = std::make_tuple(arg);
+    auto tpl_cat = std::tuple_cat(tpl, tpl_ptr);
+    return make_tpl_replaced(tpl_cat, args...); // TODO: has to be right type
+  }
 
-/// MatIterator_<T>  is a better fit than the generic argument
-template <typename Tpl, typename T, typename... Args>
-constexpr auto make_tpl_replaced(Tpl tpl, cv::MatIterator_<T> arg,
-                                 Args... args) {
-
-  auto tpl_ptr = std::make_tuple((T *)arg.ptr);
-  auto tpl_cat = std::tuple_cat(tpl, tpl_ptr);
-  return make_tpl_replaced(tpl_cat, args...);
-}
-
-/// MatConstIterator_<T> is a better fit than the generic argument
-template <typename Tpl, typename T, typename... Args>
-constexpr auto make_tpl_replaced(Tpl tpl, cv::MatConstIterator_<T> arg,
-                                 Args... args) {
-
-  auto tpl_ptr = std::make_tuple((T *)arg.ptr);
-  auto tpl_cat = std::tuple_cat(tpl, tpl_ptr);
-  return make_tpl_replaced(tpl_cat, args...);
+  else {
+    auto tpl_ptr = std::make_tuple(arg.ptr);
+    auto tpl_cat = std::tuple_cat(tpl, tpl_ptr);
+    return make_tpl_replaced(tpl_cat, args...); // TODO: has to be right type
+  }
 }
 
 /// This is how we loop through the tuple.
 /// We replace all instances of a cv::MatIterator  with its pointer.
 /// Last round in the recursive call
-template <typename Tpl, typename Arg>
-constexpr auto make_tpl_replaced(Tpl tpl, Arg arg) {
-  auto tpl_base = std::make_tuple(arg);
-  return std::tuple_cat(tpl, tpl_base);
-}
-
-/// MatIterator_<T> is a better fit than the generic argument
-template <typename Tpl, typename T>
-constexpr auto make_tpl_replaced(Tpl tpl, cv::MatIterator_<T> arg) {
-  auto tpl_ptr = std::make_tuple(arg.ptr);
-  return std::tuple_cat(tpl, tpl_ptr);
-}
-
-/// MatConstIterator_<T> is a better fit than the generic argument
-template <typename Tpl, typename T>
-constexpr auto make_tpl_replaced(Tpl tpl, cv::MatConstIterator_<T> arg) {
-  auto tpl_ptr = std::make_tuple(arg.ptr);
-  return std::tuple_cat(tpl, tpl_ptr);
-}
 
 /// These two functions replace a c++17 if constexpr.
 /// We return true in the generic case because that's the neutral element of the
 /// && operation
-template <typename Arg> bool __isContinuous_single(Arg arg) {
+template <typename Arg,
+          typename std::enable_if<!std::is_base_of<
+              cv::MatConstIterator, Arg>::value>::type * = nullptr>
+std::pair<bool, bool> __isContinuous_single(Arg arg) {
   (void)arg; // unused parameter. Only used for overload resolution
-  return true;
+  return std::make_pair(true, false);
 }
 
 /// These two functions replace a c++17 if constexpr.
 /// We return if the iterator is contiguous
-bool __isContinuous_single(cv::MatConstIterator it) {
-  return it.m->isContinuous();
+std::pair<bool, bool> __isContinuous_single(cv::MatConstIterator it) {
+  return std::make_pair(it.m->isContinuous(), true);
 }
 
-/// We loop through the touple
-template <typename Tpl> bool __isContinuous(Tpl tpl) {
+/// We loop through the touple and try to find if we can replace iterators by
+/// its pointers This is only valid, if there cv::Mat matrices described by the
+/// iterators in there, that are all contiguous in memory!
+template <typename Tpl> bool __iteratorsReplaceable(Tpl tpl) {
   bool isContinuous = true; // runtime
   bool oneElement = false;
 
@@ -99,7 +87,10 @@ template <typename Tpl> bool __isContinuous(Tpl tpl) {
 
   // Here we for loop
   for_<tupleSize>([&](auto i) {
-    isContinuous &= __isContinuous_single(std::get<i.value>(tpl));
+    std::pair<bool, bool> continuousPair =
+        __isContinuous_single(std::get<i.value>(tpl));
+    isContinuous &= continuousPair.first;
+    oneElement |= continuousPair.second;
   });
 
   return isContinuous && oneElement;
@@ -114,7 +105,7 @@ template <typename... Args> auto count_if(Args... args) {
 
   std::tuple<Args...> tpl = std::make_tuple(args...);
   constexpr size_t tupleSize = std::tuple_size<decltype(tpl)>::value;
-  bool isContinuous = __isContinuous(tpl);
+  bool isContinuous = __iteratorsReplaceable(tpl);
 
   auto replacedTpl = make_tpl_replaced(std::tuple<>{}, args...);
   if (!isContinuous) {
@@ -123,3 +114,4 @@ template <typename... Args> auto count_if(Args... args) {
     return std::count_if(std::forward<Args>(args)...);
   }
 }
+} // namespace experimental
--- src/test-main.cc
+++ src/test-main.cc
@@ -4,8 +4,51 @@
 #include <iostream>
 #include <math.h>
 
+#include <gtest/gtest.h>
 #include <random>
 
+TEST(ContinuousDetection, continuousCvMat) {
+  cv::Mat_<char> mat(10, 10, 4);
+
+  EXPECT_TRUE(experimental::__iteratorsReplaceable(std::make_tuple(
+      mat.begin(), mat.end(), [](auto a, auto b) { return a + b; })));
+
+  // cv::Mat_<T> matSub = mat(cv::Rect(4, 4, U - 10, U - 10));
+}
+
+TEST(ContinuousDetection, nonContinuousCvMat) {
+  cv::Mat_<char> mat(100, 100, 4);
+  cv::Mat_<char> matSub = mat(cv::Rect(4, 4, 90, 90));
+
+  EXPECT_FALSE(experimental::__iteratorsReplaceable(
+      std::make_tuple(matSub.begin(), matSub.end(), mat.begin(),
+                      [](auto a, auto b) { return a + b; })));
+}
+
+TEST(ContinuousDetection, vectorIterator) {
+
+  std::vector<char> mat{1, 2, 3, 4, 1};
+
+  EXPECT_FALSE(experimental::__iteratorsReplaceable(
+      std::make_tuple(mat.begin(), mat.end(), mat.begin(),
+                      [](auto a, auto b) { return a + b; })));
+}
+
+TEST(IteratorReplacement, tupleReplaceTest) {
+
+  cv::Mat_<char> mat(10, 10, 4);
+
+  auto tpl = experimental::make_tpl_replaced(std::tuple<>{}, mat.begin(),
+                                             mat.end(), mat.begin());
+
+  auto tpl_ref =
+      std::make_tuple(mat.begin().ptr, mat.end().ptr, mat.begin().ptr);
+
+  bool same = std::is_same<decltype(tpl), decltype(tpl_ref)>::value;
+  EXPECT_TRUE(same);
+}
+
+/*
 template <typename T, int U> static void BM_count_if(benchmark::State &state) {
   double lower_bound = 1;
   double upper_bound = 10000;
@@ -48,8 +91,8 @@ static void BM_count_if_forward(benchmark::State &state) {
 
     // T *beginPtr = reinterpret_cast<T *>(mat.data);
     // T *endPtr = reinterpret_cast<T *>(mat.data) + mat.cols * mat.rows;
-    transform(mat.begin(), mat.end(), mat.begin(),
-              [](T val) { return 2 * val; });
+    std::transform(mat.begin(), mat.end(), mat.begin(),
+                   [](T val) { return 2 * val; });
 
     // d = cv::countNonZero(matSub);
     benchmark::DoNotOptimize(mat);
@@ -63,3 +106,9 @@ BENCHMARK_TEMPLATE(BM_count_if, char, 4 * 100);
 BENCHMARK_TEMPLATE(BM_count_if_forward, char, 4 * 100);
 
 BENCHMARK_MAIN();
+*/
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
